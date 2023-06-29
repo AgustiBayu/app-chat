ecb4a714045953ad19bff28043ac50d30863aea8
Agusti Bayu <80197846+AgustiBayu@users.noreply.github.com>
2023-06-29 13:39:42 +0700

    new release

---
+++ apps-chat/apps-chat/src/main/java/mcrmilenial/appschat/AppsChatApplication.java
@@ -0,0 +1,13 @@
+package mcrmilenial.appschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsChatApplication.class, args);
+	}
+
+}
---
+++ apps-chat/apps-chat/src/test/java/mcrmilenial/appschat/AppsChatApplicationTests.java
@@ -0,0 +1,13 @@
+package mcrmilenial.appschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppsChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
